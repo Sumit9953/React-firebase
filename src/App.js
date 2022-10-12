c7f3f98c243376853b59747cc1d9fa96ae3dca23
Santosh kumar <sumitsantosh555@gmail.com>
2022-10-12 14:58:21 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {auth} from './firebase/init'
+import { createUserWithEmailAndPassword,onAuthStateChanged,signInWithEmailAndPassword,signOut } from "firebase/auth";
 
 function App() {
+  const[user,setUser] = React.useState({})
+  const[loading,setLoading] = React.useState(true);
+
+  React.useEffect(() =>{
+    onAuthStateChanged(auth,(user) => {
+      setLoading(false);
+      console.log(user);
+      if(user){
+        setUser(user);
+      }
+    })
+  },[]);
+
+  function register(){
+    console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com','test123')
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+  }
+  function login(){
+    signInWithEmailAndPassword(auth, 'email@email.com','test123')
+    .then(({user}) => {
+      console.log(user);
+      setUser(user);
+    })
+    .catch((error) =>{
+      console.log(error.message);
+    })
+  }
+  function logout(){
+    signOut(auth)
+    setUser({});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={register}>Register</button>
+    <button onClick={login}>Login</button>
+    <button onClick={logout}>LogOut</button>
+    {loading ? 'loading...': user.email}
     </div>
   );
 }
