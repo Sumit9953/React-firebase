b88284df5ec1966fbc8dc9552ec483daf5d9434c
Santosh kumar <sumitsantosh555@gmail.com>
2022-10-15 13:06:41 +0530

    CURD in firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import React from 'react'
 import './App.css';
-import {auth} from './firebase/init'
+import {auth , db} from './firebase/init'
+import { collection , addDoc ,getDocs,getDoc,doc,query,where, updateDoc, deleteDoc} from 'firebase/firestore';
 import { createUserWithEmailAndPassword,onAuthStateChanged,signInWithEmailAndPassword,signOut } from "firebase/auth";
 
 function App() {
   const[user,setUser] = React.useState({})
   const[loading,setLoading] = React.useState(true);
 
+  function updatePost(){
+    const ID = "GdpFpbITp5fd8XSaupS7";
+    const postRef = doc(db,"posts",ID);
+    const newPost = {
+      description: "Finish",
+      uid: "21",
+      title:"Land a $300k job"
+    };
+    updateDoc(postRef,newPost);
+  }
+
+  function Deletepost(){
+    const ID = "GdpFpbITp5fd8XSaupS7";
+    const postRef = doc(db,"posts",ID);
+    deleteDoc(postRef);
+  }
+  function createPost(){
+    const post = {
+      title: "finsed ended firebase",
+      description: " frontend simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db,"posts"),post)
+  }
+
+  async function getAllposts(){
+    const {docs} = await getDocs(collection(db,"posts"));
+    const posts = docs.map((elem) => ({...elem.data(),id:elem.id}));
+    console.log(posts);
+  }
+
+  async function getpostById(){
+    const ID = "GdpFpbITp5fd8XSaupS7";
+    const postRef = doc(db,"posts",ID);
+    const postsnap = await getDoc(postRef);
+    const post = postsnap.data()
+    console.log(post);
+  }
+
+ async function getpostbyUid(){
+    const postbyquery = await query(
+      collection(db,"posts"),
+      where("uid" , "==" ,user.uid),
+    );
+    const { docs } = await getDocs(postbyquery);
+    console.log(docs.map(doc => doc.data()));
+  }
   React.useEffect(() =>{
     onAuthStateChanged(auth,(user) => {
       setLoading(false);
-      console.log(user);
+      // console.log(user);
       if(user){
         setUser(user);
       }
@@ -47,6 +95,12 @@ function App() {
     <button onClick={login}>Login</button>
     <button onClick={logout}>LogOut</button>
     {loading ? 'loading...': user.email}
+    <button onClick={createPost}>Create post</button>
+    <button onClick={getAllposts}>Get all post</button>
+    <button onClick={getpostById}>Get Post By Id</button>
+    <button onClick={getpostbyUid}>Get Post By UId</button>
+    <button onClick={updatePost}>Update Post</button>
+    <button onClick={Deletepost}>Delete Post</button>
     </div>
   );
 }
--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore } from "firebase/firestore"
 import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0cSFVQL6VexR5IE1P1MtEVZ8_CWooxLI",
-  authDomain: "fir-practice-2b211.firebaseapp.com",
-  projectId: "fir-practice-2b211",
-  storageBucket: "fir-practice-2b211.appspot.com",
-  messagingSenderId: "452770881107",
-  appId: "1:452770881107:web:6bf07678eb2ee5f1ae70a6"
-};
+    apiKey: "AIzaSyD0cSFVQL6VexR5IE1P1MtEVZ8_CWooxLI",
+    authDomain: "fir-practice-2b211.firebaseapp.com",
+    projectId: "fir-practice-2b211",
+    storageBucket: "fir-practice-2b211.appspot.com",
+    messagingSenderId: "452770881107",
+    appId: "1:452770881107:web:6bf07678eb2ee5f1ae70a6"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore();
